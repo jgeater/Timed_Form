19b1096dfaa4cb3f19e3559c75c3ca0b1447a07b
James Geater <jgeater@outlook.com>
2017-09-19 09:30:06 -0400

    a simple timed form

---
+++ Timed_Form/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Timed_Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AllowDrop = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 17);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.MaximumSize = new System.Drawing.Size(1826, 869);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1774, 781);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Testing\r\n1\r\n2\r\n3\r\n";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.UseWaitCursor = true;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(118, 706);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.progressBar1.Minimum = 1;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(1554, 44);
+            this.progressBar1.TabIndex = 1;
+            this.progressBar1.Value = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1800, 798);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1826, 869);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(1826, 869);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Prerequisite Checks completed successfully";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Timed_Form/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+using System.Windows.Forms;
+
+namespace Timed_Form
+{
+    public partial class Form1 : Form
+    {
+          public Form1()
+        
+        {
+            InitializeComponent();
+            Timer timer1 = new Timer(); //Initialize a new Timer of name timer1
+            timer1.Tick += new EventHandler(timer1_Tick); //Link the Tick event with timer1_Tick
+            timer1.Start(); //Start the timer
+            Cursor.Hide();
+            label1.DoubleClick -= (null);
+            label1.Click -= (null);
+            label1.MouseMove -= (null);
+
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            for (int i = 1; i <= 100; i++)
+            {
+                // Wait 100 milliseconds.
+                System.Threading.Thread.Sleep(300);
+                // Report progress.
+                progressBar1.Value = i;
+            }
+
+            Environment.Exit(0);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+                        
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            Cursor.Position = new Point(Cursor.Position.X - 50, Cursor.Position.Y - 50);
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            return;
+        }
+
+        private void Control1_MouseDoubleClick(Object sender, MouseEventArgs e)
+        {
+            label1.DoubleClick -= (null);
+        }
+    }
+}
